c97cbc78472175cd4680d4d91dc2b02d7b7a92d7
relick <drrintofen@gmail.com>
2018-05-30 23:30:32 +0100

    testing

--- Usami2/main.js
+++ Usami2/main.js
@@ -70,6 +70,7 @@ client.on('message', msg => {
   if(checkPrefix(conL, config)) {
     conL.slice(1); //remove prefix from future edits
     let params = conL.split(' ');
+    console.log(params);
     if(params.length >= 0) {
       //add server to valid list
       if(params[0] === 'addserver') {
