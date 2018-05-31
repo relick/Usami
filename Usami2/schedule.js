181754f19884cea5b3e69024ba272d48e305fee0
relick <drrintofen@gmail.com>
2018-05-31 14:04:33 +0100

    x

--- Usami2/schedule.js
+++ Usami2/schedule.js
@@ -16,7 +16,7 @@ function startScheduler(msg, params, data, makeEmb) {
     data.schedule.saturday = {y:[], n:[], u:[]}; //y=can do, n=cannot do, u=unknown, will be reminded later.
     data.schedule.sunday = {y:[], n:[], u:[]};
 
-    msg.channel.send(`@everyone | ${msg.author.username} wants to schedule a D&D game.`).then(message => message.pin()).catch(console.error);
+    msg.channel.send(`everyone | ${msg.author.username} wants to schedule a D&D game.`).then(message => message.pin()).catch(console.error);
     msg.channel.send("Please tick this if you can play on Saturday and cross it if you can't.").then(message => {
         data.schedule.msat = message;
         message.react('✅');
@@ -64,15 +64,15 @@ function reactionAdded(msgrct, usr, data, makeEmb) {
         let s = data.schedule;
         let total = s.sunday.y.length + s.sunday.n.length + s.saturday.y.length + s.saturday.n.length;
         if(!transferred && total >= s.numplayers*2) {
-            let str = "@everyone | Not enough players for a game. :frowning:";
+            let str = "everyone | Not enough players for a game. :frowning:";
             if(s.sunday.y.length >= s.numplayers) {
-                str = "@everyone | Team 1, Sunday, 11am GMT.";
+                str = "everyone | Team 1, Sunday, 11am GMT.";
             } else if(s.saturday.y.length >= s.numplayers) {
-                str = "@everyone | Team 1, Saturday, 11am GMT.";
+                str = "everyone | Team 1, Saturday, 11am GMT.";
             } else if(s.sunday.y.length >= 4) {
-                str = `@everyone | Team ${s.spareteam}, Sunday, 11am GMT.`;
+                str = `everyone | Team ${s.spareteam}, Sunday, 11am GMT.`;
             } else if(s.saturday.y.length >= 4) {
-                str = `@everyone | Team ${s.spareteam}, Saturday, 11am GMT.`;
+                str = `everyone | Team ${s.spareteam}, Saturday, 11am GMT.`;
             }
             msgrct.message.channel.send(str).then(message => message.pin()).catch(console.error);
             data.schedule.active = false;
