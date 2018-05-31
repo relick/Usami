8afaf9d305c246a61d9259cb5b2212f80a084c48
relick <drrintofen@gmail.com>
2018-05-31 21:43:44 +0100

    Help update for remind

--- Usami2/help.js
+++ Usami2/help.js
@@ -6,7 +6,9 @@ Starts scheduling a D&D game. When used without extra parameters, it assumes 6 p
 A minimum of 3 players are needed to play any game.
 All players must tick, cross, or ? a schedule post for Usami to schedule anything. Usami will set reminders for anyone who picks ? for a couple days before the weekend.
 \`\`\`${data.prefix}stats\`\`\`
-Rolls you a set of 4d6kh3 stats.`);
+Rolls you a set of 4d6kh3 stats.
+\`\`\`${data.prefix}remind {timeframe} {message}\`\`\`
+Specify a time frame like this: "1 week" or "10 years". Then put a space and type your message, and the bot will remind you after the time frame has passed.`);
     msg.channel.send({embed:em});
 }
 
