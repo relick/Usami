becccac02e2a7d84adcc7e93a768eb577355a35b
relick <drrintofen@gmail.com>
2020-04-12 01:35:56 +0100

    lazily make new xp tracker command
    will give it proper features later

---
+++ Usami2/xp.js
@@ -0,0 +1,33 @@
+function initxp() {
+    if(data.xp === undefined) {
+        data.xp = { currentXP = 14000 };
+    }
+}
+
+function xp(msg, params, data, makeEmb) {
+    if(params.length < 2) {
+        msg.reply("you need to specify a command.");
+        return;
+    }
+
+    if(params[1] === "add") {
+        if(params.length < 3) {
+            msg.reply("you need to specify an amount.");
+            return;
+        }
+        let n = parseInt(params[2]);
+        if(isNaN(n)) {
+            msg.reply(`parse failed. \`${params[2]}\` is not a number.`);
+            return;
+        }
+        initxp();
+        data.xp.currentXP += n;
+    
+        msg.reply(`New party XP level is ${data.xp.currentXP}!`);
+    } else if(params[1] === "show") {
+        initxp();
+        msg.reply(`Current party XP level is ${data.xp.currentXP}!`);
+    }
+}
+
+module.exports = {message: xp};
