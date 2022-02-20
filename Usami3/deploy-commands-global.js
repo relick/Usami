394dbc46e2135f7152052bf158035da33f927471
relick <drrintofen@gmail.com>
2022-02-20 12:36:39 +0000

    Add global command deployment

---
+++ Usami3/deploy-commands-global.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const { REST } = require('@discordjs/rest');
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { Routes } = require('discord-api-types/v9');
+const { clientID, servers, token, macros, } = require('./config.json');
+
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	commands.push(command.data.toJSON());
+}
+
+for (const macro of macros) {
+	commands.push(new SlashCommandBuilder().setName(macro.trigger).setDescription(macro.reply).toJSON());
+}
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+for (const guildID of servers) {
+	rest.put(Routes.applicationGuildCommands(clientID, guildID), { body: [] })
+		.then(() => console.log(`Successfully cleared application commands in guild '${guildID}'.`))
+		.catch(console.error);
+}
+
+rest.put(Routes.applicationCommands(clientID), { body: commands })
+	.then(() => console.log(`Successfully registered application commands globally.`))
+	.catch(console.error);
